caeb28d8b563a9a63ce68b08481eb1e83240f928
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-14 15:10:41 +0300

    add gifs links  and functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GifsService } from './gifs.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GiphyComponent } from './giphy/giphy.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TrendingComponent } from './trending/trending.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { ClipsComponent } from './clips/clips.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GiphyComponent,
-    NavbarComponent
+    NavbarComponent,
+    TrendingComponent,
+    ArtistsComponent,
+    ClipsComponent,
+    StoriesComponent,
+  
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GifsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/giphy/giphy.component.ts
+++ src/app/giphy/giphy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../gifs.service';
 
 @Component({
   selector: 'app-giphy',
